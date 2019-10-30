83156f46608db778338ec43b6d4ecbdca7f3f962
Eli Bishop <eli@launchdarkly.com>
2019-10-29 20:20:11 -0700

    fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,69 @@
-const common = require('./rollup.common.config');
 const pkg = require('./package.json');
+const resolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+const babel = require('rollup-plugin-babel');
+const replace = require('@rollup/plugin-replace');
+const { uglify } = require('rollup-plugin-uglify');
+const builtins = require('rollup-plugin-node-builtins');
+const filesize = require('rollup-plugin-filesize');
 
-const config = Object.assign({}, common, {
-  input: 'src/index.js',
-  output: [
-    {
-      plugins: common.plugins,
+const env = process.env.NODE_ENV || 'development';
+const version = process.env.npm_package_version;
+
+const entryPoint = 'src/index.js';
+
+const basePlugins = [
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(env),
+    VERSION: JSON.stringify(version),
+  }),
+  builtins(),
+  resolve({
+    mainFields: ['browser', 'module', 'main'],
+    preferBuiltins: true,
+  }),
+  commonjs(),
+  babel({
+    exclude: 'node_modules/**',
+  }),
+  filesize(),
+];
+
+const webPlugins = env === 'production' ?
+  basePlugins.concat(
+    uglify()
+  ) :
+  basePlugins;
+
+const configs = [
+  {
+    input: entryPoint,
+    output: {
       name: 'LDClient-Common',
       file: process.env.NODE_ENV === 'production' ? './dist/ldclient-common.min.js' : './dist/ldclient-common.js',
       format: 'umd',
       sourcemap: true,
     },
-    { file: pkg.main, format: 'cjs', sourcemap: true },
-    { file: pkg.module, format: 'es', sourcemap: true },
-  ],
-});
+    plugins: webPlugins,
+  },
+  {
+    input: entryPoint,
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    plugins: basePlugins,
+  },
+  {
+    input: entryPoint,
+    output: {
+      file: pkg.main,
+      format: 'es',
+      sourcemap: true,
+    },
+    plugins: basePlugins,
+  },
+];
 
-module.exports = config;
+module.exports = configs;
