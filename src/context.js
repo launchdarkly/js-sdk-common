2d7056b658083503bfe78d8b526b4a99bd1de8c1
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2022-09-07 11:29:50 -0700

    [sc-160947] Switch to partial URL encoding. (#72)

--- src/context.js
+++ src/context.js
@@ -57,17 +57,33 @@ function getContextKinds(context) {
   return [];
 }
 
+/**
+ * The partial URL encoding is needed because : is a valid character in context keys.
+ *
+ * Partial encoding is the replacement of all colon (:) characters with the URL
+ * encoded equivalent (%3A) and all percent (%) characters with the URL encoded
+ * equivalent (%25).
+ * @param {string} key The key to encode.
+ * @returns {string} Partially URL encoded key.
+ */
+function encodeKey(key) {
+  if (key.includes('%') || key.includes(':')) {
+    return key.replace(/%/g, '%25').replace(/:/g, '%3A');
+  }
+  return key;
+}
+
 function getCanonicalKey(context) {
   if (context) {
     if ((context.kind === undefined || context.kind === null || context.kind === 'user') && context.key) {
       return context.key;
     } else if (context.kind !== 'multi' && context.key) {
-      return `${context.kind}:${encodeURIComponent(context.key)}`;
+      return `${context.kind}:${encodeKey(context.key)}`;
     } else if (context.kind === 'multi') {
       return Object.keys(context)
         .sort()
         .filter(key => key !== 'kind')
-        .map(key => `${key}:${encodeURIComponent(context[key].key)}`)
+        .map(key => `${key}:${encodeKey(context[key].key)}`)
         .join(':');
     }
   }
