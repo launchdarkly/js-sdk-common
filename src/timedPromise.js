5b2c4b7e361cb8cede5c6e139d702cd117eae181
Yusinto Ngadiman <yusinto@gmail.com>
2024-04-17 11:42:12 -0700

    chore: Added timedPromise and new error type.

---
+++ src/timedPromise.js
@@ -0,0 +1,18 @@
+import { LDTimeoutError } from './errors';
+
+/**
+ * Returns a promise which errors after t seconds.
+ *
+ * @param t Timeout in seconds.
+ * @param taskName Name of task being timed for logging and error reporting.
+ */
+const timedPromise = (t, taskName) =>
+  new Promise((_res, reject) => {
+    setTimeout(() => {
+      const e = `${taskName} timed out after ${t} seconds.`;
+      reject(new LDTimeoutError(e));
+      
+    }, t * 1000);
+  });
+
+export default timedPromise;
